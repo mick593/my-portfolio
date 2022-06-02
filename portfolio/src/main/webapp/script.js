cb42a6247d671c6ba07e721b8f530d9700cf2dd8
mick <micktoying@gmail.com>
2022-06-02 07:35:56 +0000

    Add photo and modify page's content.

--- portfolio/src/main/webapp/script.js
+++ portfolio/src/main/webapp/script.js
@@ -17,7 +17,7 @@
  */
 function addRandomGreeting() {
   const greetings =
-      ['Hello world!', '¡Hola Mundo!', '你好，世界！', 'Bonjour le monde!'];
+      ['Hello world!', 'Spy x Family is a great anime!', '你好，世界！', 'Bonjour le monde!', 'ดีจ้าาาา'];
 
   // Pick a random greeting.
   const greeting = greetings[Math.floor(Math.random() * greetings.length)];
