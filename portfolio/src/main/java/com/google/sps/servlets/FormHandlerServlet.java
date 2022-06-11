6596963cd083f242cef5d1492d928491e3ebab92
mick <micktoying@gmail.com>
2022-06-11 08:47:59 +0000

    Revert "use NLP score to filter out negative message"
    This reverts commit 942fee31dc0f1eaa50dd6d6629dba02870bbe7d6.

--- portfolio/src/main/java/com/google/sps/servlets/FormHandlerServlet.java
+++ portfolio/src/main/java/com/google/sps/servlets/FormHandlerServlet.java
@@ -1,8 +1,5 @@
 package com.google.sps.servlets;
 
-import com.google.cloud.language.v1.Document;
-import com.google.cloud.language.v1.LanguageServiceClient;
-import com.google.cloud.language.v1.Sentiment;
 import java.io.IOException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -20,19 +17,8 @@ public class FormHandlerServlet extends HttpServlet {
     String email = request.getParameter("email-input");
     String textValue = request.getParameter("text-input");
 
-    // use NLP score to filter out negative message
-    Document doc =
-            Document.newBuilder().setContent(textValue).setType(Document.Type.PLAIN_TEXT).build();
-    LanguageServiceClient languageService = LanguageServiceClient.create();
-    Sentiment sentiment = languageService.analyzeSentiment(doc).getDocumentSentiment();
-    float score = sentiment.getScore();
-    languageService.close();
-
-    // print a message to the log if the content is not negative.
-    if (score > 0) {
-        System.out.println("New user #" + count + " info: " + name + " " + email + " " + textValue);
-    }
-    
+    // Print the value so you can see it in the server logs.
+    System.out.println("New user #" + count + " info: " + name + " " + email + " " + textValue);
     count+=1;
     // redirect to meme song on youtube.com
     response.sendRedirect("https://www.youtube.com/watch?v=dQw4w9WgXcQ");
