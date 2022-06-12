1d77665c6b9f28e01594ce98c8b3be453ded285a
mick <micktoying@gmail.com>
2022-06-12 08:11:18 +0000

    store message from visitor to datastore

--- portfolio/src/main/java/com/google/sps/servlets/FormHandlerServlet.java
+++ portfolio/src/main/java/com/google/sps/servlets/FormHandlerServlet.java
@@ -1,8 +1,15 @@
 package com.google.sps.servlets;
 
-import com.google.cloud.language.v1.Document;
-import com.google.cloud.language.v1.LanguageServiceClient;
-import com.google.cloud.language.v1.Sentiment;
+import com.google.cloud.datastore.Datastore;
+import com.google.cloud.datastore.DatastoreOptions;
+import com.google.cloud.datastore.Entity;
+import com.google.cloud.datastore.FullEntity;
+import com.google.cloud.datastore.IncompleteKey;
+import com.google.cloud.datastore.KeyFactory;
+// import com.google.cloud.language.v1.Document;
+// import com.google.cloud.language.v1.LanguageServiceClient;
+// import com.google.cloud.language.v1.Sentiment;
+
 import java.io.IOException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -11,7 +18,6 @@ import javax.servlet.http.HttpServletResponse;
 
 @WebServlet("/form-handler")
 public class FormHandlerServlet extends HttpServlet {
-  private int count = 0;
   @Override
   public void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
 
@@ -19,21 +25,22 @@ public class FormHandlerServlet extends HttpServlet {
     String name = request.getParameter("name-input");
     String email = request.getParameter("email-input");
     String textValue = request.getParameter("text-input");
+    long timestamp = System.currentTimeMillis();
 
-    // use NLP score to filter out negative message
-    Document doc =
-            Document.newBuilder().setContent(textValue).setType(Document.Type.PLAIN_TEXT).build();
-    LanguageServiceClient languageService = LanguageServiceClient.create();
-    Sentiment sentiment = languageService.analyzeSentiment(doc).getDocumentSentiment();
-    float score = sentiment.getScore();
-    languageService.close();
 
-    // print a message to the log if the content is not negative.
-    if (score > 0) {
-        System.out.println("New user #" + count + " info: " + name + " " + email + " " + textValue);
-    }
+    // store a message to the database.
+    Datastore datastore = DatastoreOptions.getDefaultInstance().getService();
+    KeyFactory keyFactory = datastore.newKeyFactory().setKind("Message");
+    FullEntity<IncompleteKey> msg =
+        Entity.newBuilder(keyFactory.newKey())
+            .set("name", name)
+            .set("email", email)
+            .set("text", textValue)
+            .set("timestamp", timestamp)
+            .build();
+    datastore.put(msg);
     
-    count+=1;
+    
     // redirect to meme song on youtube.com
     response.sendRedirect("https://www.youtube.com/watch?v=dQw4w9WgXcQ");
 
